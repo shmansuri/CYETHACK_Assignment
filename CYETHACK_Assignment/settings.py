b5f4141491fb6925c24960722836ef3968d13dbd
shmansuri <shmansuri86515@gmail.com>
2025-12-25 11:21:41 +0530

    allow host

--- CYETHACK_Assignment/settings.py
+++ CYETHACK_Assignment/settings.py
@@ -15,9 +15,9 @@ load_dotenv(BASE_DIR / ".env")
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
